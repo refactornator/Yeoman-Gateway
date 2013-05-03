e624763d97396a3c7ad9820cb56babbabdd6b160
William Lindner <wlindner522@gmail.com>
2013-05-02 21:49:00 -0400

    Defined a style for the login screen.

--- app/scripts/views/Login-view.js
+++ app/scripts/views/Login-view.js
@@ -4,6 +4,7 @@ define(['marionette','templates'], function (Marionette, JST) {
     'use strict';
 
     var LoginView = Marionette.ItemView.extend({
+        className: 'hero-unit',
         template: JST['app/scripts/templates/Login.ejs']
     });
 
