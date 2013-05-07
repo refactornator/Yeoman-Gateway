6be61d8e7b0c1e562645f544490d71002477c4b5
William Lindner <wlindner522@gmail.com>
2013-05-07 08:27:00 -0400

    Fixed Gateway controller to load models and views.

--- app/scripts/controllers/gateway-controller.js
+++ app/scripts/controllers/gateway-controller.js
@@ -1,23 +1,19 @@
 /*global define*/
 
-define(['gateway'], function (Gateway) {
+define(['gateway','models/user-model','views/Login-view','views/Register-view'], function (Gateway, UserModel, LoginView, RegisterView) {
     'use strict';
 
     var routerController = {
         login: function() {
-            require(['models/user-model', 'views/Login-view'], function(UserModel, LoginView){
-                Gateway.main.show(new LoginView({
-                    model : new UserModel()
-                }));
-            })
+            Gateway.main.show(new LoginView({
+                model : new UserModel()
+            }));
         },
 
         signup: function() {
-            require(['models/user-model', 'views/Register-view'], function(UserModel, RegisterView){
-                Gateway.main.show(new RegisterView({
-                    model : new UserModel()
-                }));
-            });
+            Gateway.main.show(new RegisterView({
+                model : new UserModel()
+            }));
         }
     };
 
